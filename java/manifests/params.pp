0e2d79657351fd034981696a315fb904371a9b90
Mouna <mounareddy.m@gmail.com>
2017-10-04 19:52:26 +0000

    Initial Commit

---
+++ java/manifests/params.pp
@@ -0,0 +1,4 @@
+class java::params {
+        $java_base = "/opt/java"
+        $java_version = "1.8.0_144"
+}
---
+++ ntp/manifests/service.pp
@@ -0,0 +1,17 @@
+class ntp::service(
+	String $service_name 	= $ntp::service_name,
+	String $service_ensure	= $ntp::service_ensure,
+	Boolean $service_enable = $ntp::service_enable,
+	Boolean $service_hasstatus = $ntp::service_hasstatus,
+	Boolean $service_hasrestart = $ntp::service_hasrestart,
+)
+{
+	service { 'ntp':
+		name		=> $service_name,
+		ensure 		=> $service_ensure,
+		enable 		=> $service_enable,
+		hasstatus 	=> $service_hasstatus,
+		hasrestart	=> $service_hasrestart,
+	}
+	
+}
