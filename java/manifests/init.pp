0257a531a6681bdb94ac158a4ce269272cb0d6c4
root <root@agent1.cdl.local>
2017-10-12 07:00:40 +0000

    changes to java file

--- java/manifests/init.pp
+++ java/manifests/init.pp
@@ -1,40 +1,34 @@
-class java{
+class java inherits java::params {
 
-      require java::params 
-      
 	file { "${java::params::java_base}":
                 ensure => 'directory',
                 mode => '0644',
                 owner => 'root',
                 group => 'root',
-                alias => "base-dir",
         }
         file { "${java::params::java_base}/jdk${java::params::java_version}.tar.gz":
                 mode => '0644',
                 owner => 'root',
                 group => 'root',
                 source => "puppet:///modules/java/jdk${java::params::java_version}.tar.gz",
-                alias => "tar-file",
-                require => File["base-dir"],
-                before => Exec["extract-tar"],
+                require => File[$::java::params::java_base],
+                before => Exec["untar-jdk${java::params::java_version}.tar.gz"],
 
         }
-        exec { "untar jdk${java::params::java_version}.tar.gz":
+        exec { "untar-jdk${java::params::java_version}.tar.gz":
                 command => "/bin/tar -xzvf jdk${java::params::java_version}.tar.gz",
                 cwd     => "${java::params::java_base}",
                 creates => "${java::params::java_base}/jdk${java::params::java_version}",
-                alias => "extract-tar",
                 refreshonly => true,
-                subscribe => File["tar-file"],
-                before => File["java-dir"],
+                subscribe => File["${java::params::java_base}/jdk${java::params::java_version}.tar.gz"],
+                before => File["${java::params::java_base}/jdk${java::params::java_version}"],
         }
         file { "${java::params::java_base}/jdk${java::params::java_version}":
                 ensure => 'directory',
                 mode => '0644',
                 owner => 'root',
                 group => 'root',
-                alias => "java-dir",
-                require => Exec["extract-tar"],
+                require => Exec["untar-jdk${java::params::java_version}.tar.gz"],
         }
         file { "/etc/profile.d/java_home.sh":
                 ensure => present,
@@ -42,6 +36,7 @@ class java{
                 mode => '0755',
                 owner => root,
                 group => root,
-                require => File["java-dir"],
+                require => File["${java::params::java_base}/jdk${java::params::java_version}"],
         }
+
 }
